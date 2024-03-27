7031e34b47f11251435ff12c423003b1656593db
jsaouma <jsaouma@css.edu>
2024-03-27 18:16:10 -0500

    Changed port name

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ app.get('/cars-data', isAuthenticated, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
